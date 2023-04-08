a045a796f3c59799ebcc40ea6d3993b218738f79
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2023-04-08 13:31:42 +0300

    Divide folders and files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductDetails from "./pages/ProductDetails";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+import Layout from "./components/Layout";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/:product-id" element={<ProductDetails />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
