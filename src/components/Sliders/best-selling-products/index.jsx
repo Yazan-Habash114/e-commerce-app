7a8e8fc3178c5eeeb4731ace9f787552ad4af690
Ibraheem Shawhnee <i.shawahneh@student.aaup.edu>
2023-04-17 01:11:01 +0300

    Added Big title and View All button  {"h1 element" & "ColorButton"}, add onOff prop inside card to discount element

--- src/components/Sliders/best-selling-products/index.jsx
+++ src/components/Sliders/best-selling-products/index.jsx
@@ -2,21 +2,22 @@ import { Container, Box } from "@mui/material";
 import React from "react";
 import TitleTag from "../../SharedComponents/TitleTag/index";
 import SubTitle from "../../SharedComponents/SubTitle";
-
+import ColorButtons from "../../SharedComponents/Button";
 const BestSellingProduct = () => {
-
   return (
     <Container>
-      <Box sx={{display: "flex",
-      alignItems: "center",}}>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
         <Box>
           <TitleTag />
         </Box>
-        <Box sx={{paddingLeft: 1}}>
+        <Box sx={{ paddingLeft: 1 }}>
           <SubTitle SubTitle="This Month" />
         </Box>
       </Box>
-     
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <h1>Best Selling Products</h1>
+        <ColorButtons type={"red"} innerText={"View All"} />
+      </Box>
     </Container>
   );
 };
