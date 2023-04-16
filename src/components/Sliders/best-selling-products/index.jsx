5c7f9e9f7c438c96b42ffa7bc60c6c83350311c7
Ibraheem Shawhnee <i.shawahneh@student.aaup.edu>
2023-04-17 01:14:47 +0300

    Added Cards

--- src/components/Sliders/best-selling-products/index.jsx
+++ src/components/Sliders/best-selling-products/index.jsx
@@ -1,7 +1,9 @@
 import { Container, Box } from "@mui/material";
+import { mockData } from "../../../data";
 import React from "react";
 import TitleTag from "../../SharedComponents/TitleTag/index";
 import SubTitle from "../../SharedComponents/SubTitle";
+import ProductCard from "../../SharedComponents/Card";
 import ColorButtons from "../../SharedComponents/Button";
 const BestSellingProduct = () => {
   return (
@@ -17,6 +19,24 @@ const BestSellingProduct = () => {
       <Box sx={{ display: "flex", justifyContent: "space-between" }}>
         <h1>Best Selling Products</h1>
         <ColorButtons type={"red"} innerText={"View All"} />
+      </Box>
+      <Box
+        sx={{ display: "flex", justifyContent: "space-between", paddingTop: 5 }}
+      >
+        {mockData.map(
+          (item, index) =>
+            index < 4 && (
+              <Box key={item.id}>
+                <ProductCard
+                  image={item.image}
+                  title={item.title}
+                  rating={item.rating}
+                  price={item.price}
+                  onOff={false}
+                />
+              </Box>
+            )
+        )}
       </Box>
     </Container>
   );
