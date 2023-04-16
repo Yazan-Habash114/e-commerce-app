9569cb542ae5dbc0a6d1f7894320178e3011e09c
Ibraheem Shawhnee <i.shawahneh@student.aaup.edu>
2023-04-17 01:18:28 +0300

    Added Themes for elements

--- src/components/Sliders/best-selling-products/index.jsx
+++ src/components/Sliders/best-selling-products/index.jsx
@@ -5,24 +5,39 @@ import TitleTag from "../../SharedComponents/TitleTag/index";
 import SubTitle from "../../SharedComponents/SubTitle";
 import ProductCard from "../../SharedComponents/Card";
 import ColorButtons from "../../SharedComponents/Button";
+import { createTheme } from "@mui/material/styles";
 const BestSellingProduct = () => {
+  const theme = createTheme({
+    TitleTheme: {
+      display: "flex",
+      alignItems: "center",
+    },
+    BigTitleTheme: { display: "flex", justifyContent: "space-between" },
+    SubTitleTheme: {
+      paddingLeft: 1,
+    },
+    CardTheme: {
+      display: "flex",
+      justifyContent: "space-between",
+      paddingTop: 5,
+    },
+  });
+
   return (
     <Container>
-      <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={theme.TitleTheme}>
         <Box>
           <TitleTag />
         </Box>
-        <Box sx={{ paddingLeft: 1 }}>
+        <Box sx={theme.SubTitleTheme}>
           <SubTitle SubTitle="This Month" />
         </Box>
       </Box>
-      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+      <Box sx={theme.BigTitleTheme}>
         <h1>Best Selling Products</h1>
         <ColorButtons type={"red"} innerText={"View All"} />
       </Box>
-      <Box
-        sx={{ display: "flex", justifyContent: "space-between", paddingTop: 5 }}
-      >
+      <Box sx={theme.CardTheme}>
         {mockData.map(
           (item, index) =>
             index < 4 && (
