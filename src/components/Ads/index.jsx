ecfc767ccfcf1be1e91cc4eb587b5bde8723f0cf
Ibraheem Shawhnee <i.shawahneh@student.aaup.edu>
2023-04-17 21:52:31 +0300

    Added subtitle

---
+++ src/components/Ads/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SubTitle from "../SharedComponents/SubTitle";
+import { Box } from "@mui/material";
+const Ads = () => {
+  return (
+    <div>
+      <Box sx={{ color: "#00000" }}>
+        <SubTitle SubTitle="Hello" />
+      </Box>
+    </div>
+  );
+};
+
+export default Ads;
