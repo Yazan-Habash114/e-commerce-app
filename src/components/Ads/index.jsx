03883286098813a99f5bb519e8adaf0aaaf6029c
Ibraheem Shawhnee <i.shawahneh@student.aaup.edu>
2023-04-18 02:30:51 +0300

    Added Themes to the elements

--- src/components/Ads/index.jsx
+++ src/components/Ads/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Box, Avatar, CardMedia } from "@mui/material";
 import JBL from "../../assets/Ads/JBL_BOOMBOX.png";
 import ColorButtons from "../SharedComponents/Button";
-import createTheme from "@mui/material/styles/createTheme";
+import { createTheme } from "@mui/material/styles";
 const Ads = () => {
   const theme = createTheme({
     BlackBoxTheme: {
@@ -11,168 +11,81 @@ const Ads = () => {
       backgroundColor: "#000000",
       display: "flex",
     },
+    CategoriesTheme: {
+      color: "#00FF66",
+      paddingTop: 4,
+      fontSize: 16,
+    },
+    BigTitleTheme: {
+      color: "#FFFFFF",
+      fontSize: 48,
+      fontWeight: 600,
+      lineHeight: 1.5,
+      letterSpacing: "0.04em",
+    },
+    BoxAvatarWrapperTheme: {
+      display: "flex",
+      paddingTop: 3,
+      margin: "0 -24px",
+    },
+    AvatarTheme: {
+      width: 62,
+      height: 62,
+      textAlign: "center",
+      fontSize: 16,
+      backgroundColor: "#FFFFFF",
+      color: "#000000",
+      margin: "0 24px",
+      display: "block",
+    },
+    NumberBoxTheme: {
+      alignItems: "center",
+      textAlign: "center",
+      lineHeight: 2.5,
+      fontWeight: 600,
+    },
+    TimeTheme: {
+      alignItems: "center",
+      textAlign: "center",
+      lineHeight: 0,
+      fontSize: 11,
+    },
+    ButtonTheme: {
+      paddingTop: 4,
+    },
+    ImageTheme: {
+      paddingTop: 4,
+    },
   });
   return (
     <Container sx={theme.BlackBoxTheme}>
       <Container>
-        <Box sx={{ color: "#00FF66", paddingTop: 4, fontSize: 16 }}>
-          Categories
-        </Box>
-        <Box
-          sx={{
-            color: "#FFFFFF",
-            fontSize: 48,
-            fontWeight: 600,
-            lineHeight: 1.5,
-            letterSpacing: "0.04em",
-          }}
-        >
-          Enhance Your Music Experience
-        </Box>
-        <Box sx={{ display: "flex", paddingTop: 3, margin: "0 -24px" }}>
-          <Avatar
-            sx={{
-              width: 62,
-              height: 62,
-              textAlign: "center",
-              fontSize: 16,
-              backgroundColor: "#FFFFFF",
-              color: "#000000",
-
-              margin: "0 24px",
-              display: "block",
-            }}
-          >
-            <Box
-              sx={{
-                alignItems: "center",
-                textAlign: "center",
-                lineHeight: 2.5,
-                fontWeight: 600,
-              }}
-            >
-              23
-            </Box>
-            <Box
-              sx={{
-                alignItems: "center",
-                textAlign: "center",
-                lineHeight: 0,
-                fontSize: 11,
-              }}
-            >
-              Hours
-            </Box>
+        <Box sx={theme.CategoriesTheme}>Categories</Box>
+        <Box sx={theme.BigTitleTheme}>Enhance Your Music Experience</Box>
+        <Box sx={theme.BoxAvatarWrapperTheme}>
+          <Avatar sx={theme.AvatarTheme}>
+            <Box sx={theme.NumberBoxTheme}>23</Box>
+            <Box sx={theme.TimeTheme}>Hours</Box>
           </Avatar>
-          <Avatar
-            sx={{
-              width: 62,
-              height: 62,
-              textAlign: "center",
-              fontSize: 16,
-              backgroundColor: "#FFFFFF",
-              color: "#000000",
-
-              margin: "0 24px",
-              display: "block",
-            }}
-          >
-            <Box
-              sx={{
-                alignItems: "center",
-                textAlign: "center",
-                lineHeight: 2.5,
-                fontWeight: 600,
-              }}
-            >
-              05
-            </Box>
-            <Box
-              sx={{
-                alignItems: "center",
-                textAlign: "center",
-                lineHeight: 0,
-                fontSize: 11,
-              }}
-            >
-              Days
-            </Box>
+          <Avatar sx={theme.AvatarTheme}>
+            <Box sx={theme.NumberBoxTheme}>05</Box>
+            <Box sx={theme.TimeTheme}>Days</Box>
           </Avatar>
-          <Avatar
-            sx={{
-              width: 62,
-              height: 62,
-              textAlign: "center",
-              fontSize: 16,
-              backgroundColor: "#FFFFFF",
-              color: "#000000",
-
-              margin: "0 24px",
-              display: "block",
-            }}
-          >
-            <Box
-              sx={{
-                alignItems: "center",
-                textAlign: "center",
-                lineHeight: 2.5,
-                fontWeight: 600,
-              }}
-            >
-              59
-            </Box>
-            <Box
-              sx={{
-                alignItems: "center",
-                textAlign: "center",
-                lineHeight: 0,
-                fontSize: 11,
-              }}
-            >
-              Minuets
-            </Box>
+          <Avatar sx={theme.AvatarTheme}>
+            <Box sx={theme.NumberBoxTheme}>59</Box>
+            <Box sx={theme.TimeTheme}>Minuets</Box>
           </Avatar>
-          <Avatar
-            sx={{
-              width: 62,
-              height: 62,
-              textAlign: "center",
-              fontSize: 16,
-              backgroundColor: "#FFFFFF",
-              color: "#000000",
-
-              margin: "0 24px",
-              display: "block",
-            }}
-          >
-            <Box
-              sx={{
-                alignItems: "center",
-                textAlign: "center",
-                lineHeight: 2.5,
-                fontWeight: 600,
-              }}
-            >
-              35
-            </Box>
-            <Box
-              sx={{
-                alignItems: "center",
-                textAlign: "center",
-                lineHeight: 0,
-                fontSize: 11,
-              }}
-            >
-              Seconds
-            </Box>
+          <Avatar sx={theme.AvatarTheme}>
+            <Box sx={theme.NumberBoxTheme}>35</Box>
+            <Box sx={theme.TimeTheme}>Seconds</Box>
           </Avatar>
         </Box>
-        <Box sx={{ paddingTop: 4 }}>
+        <Box sx={theme.ButtonTheme}>
           <ColorButtons type={"Green"} innerText={"Buy Now!"} />
         </Box>
       </Container>
       <Container>
-        <CardMedia component={"img"} image={JBL} sx={{ paddingTop: 4 }} />
+        <CardMedia component={"img"} image={JBL} sx={theme.ImageTheme} />
       </Container>
     </Container>
   );
