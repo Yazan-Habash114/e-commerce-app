3aa7f2dc5f7152cf335cc55096fad65893f68c71
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2023-04-19 17:22:02 +0300

    Edit folders and arrange code

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BottomHeader from "./Header/BottomHeader";
+import Ad from "../Ad";
+
+const Header = () => {
+  return (
+    <>
+      <BottomHeader />
+      <Ad />
+    </>
+  );
+};
+
+export default Header;
